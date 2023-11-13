c8ef729a74be212aef368b882e97ec7862d9c5dd
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-13 15:51:06 +0300

    Added common types

---
+++ include/commonStructures.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <chrono>
+#include <thread>
+
+namespace TP {
+    using CallID = long long;
+}
+
+enum class CallStatus {
+    awaiting,
+    completed,
+    rejected
+};
+
+
+struct CDR {
+    std::time_t startTime;
+    std::time_t endTime;
+    TP::CallID callID;
+    CallStatus status;
+    std::thread::id operatorID;
+    std::chrono::duration<int> callDuration;
+};
