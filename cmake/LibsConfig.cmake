627c051ca5aeb97468cc7dbac47664941220e103
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-15 16:28:46 +0300

    Trying to resolve problem with boost at github actions

--- cmake/LibsConfig.cmake
+++ cmake/LibsConfig.cmake
@@ -43,7 +43,7 @@ function(DownloadAndUseLibs LIGHTWEIGHTBABY)
         message("Using system spdlog library")
     endif()
 
-    find_package(Boost COMPONENTS system date_time QUIET)
+    find_package(Boost COMPONENTS system date_time REQUIRED)
     if(NOT Boost_FOUND)
         set(GIT_TAG boost-1.83.0)
         if(LIGHTWEIGHTBABY)
