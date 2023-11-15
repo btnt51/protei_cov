2fa9e9a30f27c0b6295f96e7ff2fdb76eb202317
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-15 15:59:37 +0300

    Seems like i resolved problem with shell script

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 mkdir ./build
 cd ./build
-cmake -DCMAKE_BUILD_TYPE=$1 -DPATCH_VERSION=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
+cmake  -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
 cmake --build .
 cmake --build . --target $5
-if [ "$6" -eq  "0" ]
-  then
-    exit 0
-  else
-    cmake -E tar cfv ./$6.zip --format=7zip ./$5
+if [ -n "$6" ]; then
+  cmake -E tar cfv ./$6.zip --format=7zip ./$5
 fi
