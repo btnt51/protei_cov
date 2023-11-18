ad96a80362d310c9c2bd3bc4ed2639f2e6280fbd
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-18 10:24:52 +0300

    Added functionality to pick cxx_compiler to build.sh as 7th arg and updated build workflow

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 mkdir ./build
 cd ./build
-cmake  -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
+if [ -n "$7" ]; then
+  cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 ../
+else
+  cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
+fi
 cmake --build .
 cmake --build . --target $5
 if [ -n "$6" ]; then
