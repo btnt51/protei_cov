77f72bee49ded6bca495caeb63047d0fb2de17d7
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-27 14:44:14 +0300

    Updated shell script

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ cd ./build
 
 if [ -n "$7" ]; then
   if [ -n "${10}" ]; then
-    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 \
-      -DBoost_INCLUDE_DIR=$8 -DBoost_LIBRARY_DIRS=$9 -DBoost_ROOT=${10} ../
+    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 -DBoost_INCLUDE_DIR=$8 -DBoost_LIBRARY_DIRS=$9 -DBoost_ROOT=${10} ../
   else
     cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 ../
   fi
@@ -17,6 +16,6 @@ fi
 cmake --build .
 cmake --build . --target $5
 
-if [ -n "$6" ]; then
+if [ -n "$6" ] && [ "$6" != "test" ]; then
   cmake -E tar cfv ./$6.zip --format=7zip ./$5
 fi
