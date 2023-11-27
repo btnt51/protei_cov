2181f16b9093f22169e0d75e6fccc34bf099c865
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-27 14:38:49 +0300

    Updated build.sh script. Added arguments with boost pathes for cmake

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
 mkdir ./build
 cd ./build
+
 if [ -n "$7" ]; then
-  cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 ../
+  if [ -n "${10}" ]; then
+    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 \
+      -DBoost_INCLUDE_DIR=$8 -DBoost_LIBRARY_DIRS=$9 -DBoost_ROOT=${10} ../
+  else
+    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 ../
+  fi
 else
   cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
 fi
+
 cmake --build .
 cmake --build . --target $5
+
 if [ -n "$6" ]; then
   cmake -E tar cfv ./$6.zip --format=7zip ./$5
 fi
