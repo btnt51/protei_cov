3a523eac03e3bf1a6288f3beddc7b76b8dc08667
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-28 10:58:11 +0300

    Added queue declaration/defenition

--- include/queue.hpp
+++ include/queue.hpp
@@ -1,4 +1,31 @@
 #ifndef PROTEI_COV_QUEUE_HPP
 #define PROTEI_COV_QUEUE_HPP
+#include <memory>
+#include <queue>
+#include <tuple>
 
+#include "commonStructures.hpp"
+#include "interfaces.hpp"
+namespace TP {
+class Queue : public IQueue {
+public:
+    explicit Queue(int size);
+
+    std::pair<std::shared_ptr<ITask>, CallID> back() override;
+
+    std::pair<std::shared_ptr<ITask>, CallID> front() override;
+
+    [[nodiscard]] bool empty() const override;
+
+    [[nodiscard]] bool push(std::pair<std::shared_ptr<ITask>, CallID>&& taskPair) override;
+
+    void pop() override;
+
+    void update(int size) override;
+
+private:
+    std::vector<std::pair<std::shared_ptr<ITask>, CallID>> queue_;
+    std::size_t sizeOfQueue;
+};
+}
 #endif // PROTEI_COV_QUEUE_HPP
--- src/queue.cpp
+++ src/queue.cpp
@@ -1,1 +1,48 @@
 #include "queue.hpp"
+
+using namespace TP;
+
+Queue::Queue(int size) :
+    IQueue(size), sizeOfQueue(size) {
+    queue_.reserve(size);
+}
+
+std::pair<std::shared_ptr<ITask>, CallID> Queue::back() {
+    return queue_.back();
+}
+
+std::pair<std::shared_ptr<ITask>, CallID> Queue::front() {
+    return queue_.front();
+}
+
+bool Queue::empty() const {
+    return queue_.empty();
+}
+
+bool Queue::push(std::pair<std::shared_ptr<ITask>, CallID>&& taskPair) {
+    for(auto it = queue_.begin(); it != queue_.end(); it++) {
+        if(it->first->getNumber() == taskPair.first->getNumber()) {
+            std::rotate(it, it + 1, queue_.end());
+            Result r;
+            r.callDuration = std::chrono::seconds{0};
+            r.callID = it->second;
+            r.status = CallStatus::Duplication;
+            it->first->promise_->set_value(r);
+            break;
+        }
+    }
+    if(queue_.size() < sizeOfQueue) {
+        queue_.push_back(taskPair);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Queue::pop() {
+    queue_.erase(queue_.cbegin());
+}
+
+void Queue::update(int size) {
+    sizeOfQueue = size;
+}
