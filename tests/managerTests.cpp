26031de75260ea296a125b598d18360ce548116e
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-28 12:34:53 +0300

    Updated tests for manager

--- tests/managerTests.cpp
+++ tests/managerTests.cpp
@@ -3,6 +3,19 @@
 #include "manager.hpp"
 
 #include "interfaces.hpp"
+#include "threadpool.hpp"
+
+class MockQueue : public TP::IQueue {
+public:
+    MockQueue(int size) : TP::IQueue(size) { }
+    MOCK_METHOD((std::pair<std::shared_ptr<TP::ITask>, TP::CallID>), back, (),(override));
+    MOCK_METHOD((std::pair<std::shared_ptr<TP::ITask>, TP::CallID>), front, (),(override));
+    MOCK_METHOD(bool, empty, (), (const, override));
+    MOCK_METHOD(bool, push, ((std::pair<std::shared_ptr<TP::ITask>, TP::CallID>&& taskPair)), (override));
+    MOCK_METHOD(void, pop, (), (override));
+    MOCK_METHOD(void, update, (int size), (override));
+
+};
 
 class MockConfig : public utility::IConfig {
 public:
@@ -20,20 +33,23 @@ public:
 // Mock для IThreadPool
 class MockThreadPool : public TP::IThreadPool {
 public:
-    MockThreadPool(unsigned amountOfThreads) : TP::IThreadPool(amountOfThreads) {}
+    MockThreadPool(unsigned amountOfThreads, unsigned sizeOfQueue) : TP::IThreadPool(amountOfThreads, sizeOfQueue) {}
 
     MOCK_METHOD((std::pair<TP::CallID, std::future<Result>>), add_task, (std::shared_ptr<TP::ITask> task), (override));
     MOCK_METHOD(void, stop, (), (override));
     MOCK_METHOD(void, start, (), (override));
     MOCK_METHOD(void, transferTaskQueue, (const std::shared_ptr<TP::IThreadPool>& oldThreadPool), (override));
-    MOCK_METHOD(void, writeCDR,(CDR& cdr), (override));
+    MOCK_METHOD(void, writeCDR, (CDR& cdr), (override));
+    MOCK_METHOD(void, setTaskQueue, (std::shared_ptr<TP::IQueue> task_queue), (override));
 };
 
 class ManagerTest : public testing::Test {
 protected:
     void SetUp() override {
         mockConfig = std::make_shared<MockConfig>("base.json");
-        mockThreadPool = std::make_shared<MockThreadPool>(2);
+        mockThreadPool = std::make_shared<MockThreadPool>(2, 4);
+        mockQueue = std::make_shared<MockQueue>(4);
+        mockThreadPool->setTaskQueue(mockQueue);
         manager = std::make_shared<Manager>(mockConfig, mockThreadPool);
         mockConfig->setManager(manager);
     }
@@ -45,14 +61,14 @@ protected:
     std::shared_ptr<Manager> manager;
     std::shared_ptr<MockThreadPool> mockThreadPool;
     std::shared_ptr<MockConfig> mockConfig;
+    std::shared_ptr<MockQueue> mockQueue;
 };
 
 
 //Тест конструктора Manager
 TEST_F(ManagerTest, ConstructorTest) {
-    //EXPECT_CALL(*mockConfig, setManager(manager)).Times(1);
     EXPECT_CALL(*mockConfig, getMinMax()).WillOnce(::testing::Return(std::make_pair(10,20)));
-    auto mockThreadPool1 = std::make_shared<MockThreadPool>(2);
+    auto mockThreadPool1 = std::make_shared<MockThreadPool>(2, 4);
     auto manager1 = std::make_shared<Manager>(mockConfig, mockThreadPool1);
 
     std::cout << "Checking if manager is not nullptr" << std::endl;
@@ -60,16 +76,9 @@ TEST_F(ManagerTest, ConstructorTest) {
 }
 
 TEST_F(ManagerTest, SetThreadPool) {
-    EXPECT_CALL(*mockThreadPool, start()).Times(1);
     EXPECT_CALL(*mockThreadPool, transferTaskQueue(::testing::_)).Times(1);
     EXPECT_CALL(*mockThreadPool, stop()).Times(1);
     manager->setNewThreadPool(mockThreadPool);
-}
-
-TEST_F(ManagerTest, UpdateFunction) {
-    EXPECT_CALL(*mockConfig, getMinMax()).WillOnce(::testing::Return(std::make_pair(10,20)));
-    EXPECT_CALL(*mockConfig, getAmountOfOperators()).WillOnce(::testing::Return(2));
-    manager->update();
 }
 
 TEST_F(ManagerTest, threadPoolStartTest) {
@@ -86,4 +95,17 @@ TEST_F(ManagerTest, AddTaskTest) {
     EXPECT_CALL(*mockThreadPool, add_task(::testing::_)).WillOnce(::testing::Return(std::make_pair(10, std::future<Result>())));
     manager->addTask("test_num");
 }
+
+TEST_F(ManagerTest, UpdateFunction) {
+    EXPECT_CALL(*mockConfig, getMinMax()).WillOnce(::testing::Return(std::make_pair(10,20)));
+    EXPECT_CALL(*mockConfig, getAmountOfOperators()).WillOnce(::testing::Return(2));
+    EXPECT_CALL(*mockConfig, getSizeOfQueue()).Times(2).WillRepeatedly(::testing::Return(4));
+    EXPECT_CALL(*mockQueue, update(::testing::_)).Times(1);
+    auto threadPool = std::make_shared<TP::ThreadPool>(2, 4);
+    manager->setNewThreadPool(threadPool);
+    threadPool->setTaskQueue(mockQueue);
+    manager->update();
+}
+
+
 
