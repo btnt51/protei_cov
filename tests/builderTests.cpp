0a86a50c46e25be5bc076e1ccbe9cf5dd8e0e719
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-29 14:13:23 +0300

    Added tests for Builder

--- tests/builderTests.cpp
+++ tests/builderTests.cpp
@@ -1,3 +1,36 @@
-//
-// Created by Евгений Байлов on 29.11.2023.
-//
+#include <gtest/gtest.h>
+#include "builder.hpp"
+
+class ManagerBuilderTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        managerBuilder = std::make_shared<ManagerBuilder>();
+    }
+
+    void TearDown() override {
+
+    }
+
+    std::shared_ptr<ManagerBuilder> managerBuilder;
+};
+
+TEST_F(ManagerBuilderTest, BuildLoggerTest) {
+    ASSERT_NE(managerBuilder->BuildLogger(), nullptr);
+
+}
+
+TEST_F(ManagerBuilderTest, BuildConfigTest) {
+    // Аналогично, напишите тест для метода BuildConfig
+    managerBuilder->BuildLogger();
+    ASSERT_NE(managerBuilder->BuildConfig("base.json"), nullptr);
+}
+
+TEST_F(ManagerBuilderTest, BuildThreadPoolTest) {
+    managerBuilder->BuildLogger();
+    managerBuilder->BuildConfig("base.json");
+    ASSERT_NE(managerBuilder->BuildThreadPool(), nullptr);
+}
+
+TEST_F(ManagerBuilderTest, ConstructTest) {
+    ASSERT_NE(managerBuilder->Construct("base.json"), nullptr);
+}
