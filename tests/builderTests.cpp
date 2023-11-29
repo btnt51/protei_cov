f74eca9d519d26ac7cb090c13aa977aeae48e581
Evgeniy btnt51 <prog@btnt51.ru>
2023-11-29 14:34:53 +0300

    Updated tests

--- tests/builderTests.cpp
+++ tests/builderTests.cpp
@@ -1,5 +1,6 @@
-#include <gtest/gtest.h>
 #include "builder.hpp"
+#include <gtest/gtest.h>
+#include <spdlog/async.h>
 
 class ManagerBuilderTest : public ::testing::Test {
 protected:
@@ -15,22 +16,26 @@ protected:
 };
 
 TEST_F(ManagerBuilderTest, BuildLoggerTest) {
+    spdlog::init_thread_pool(8192, 1);
     ASSERT_NE(managerBuilder->BuildLogger(), nullptr);
 
 }
 
 TEST_F(ManagerBuilderTest, BuildConfigTest) {
+    spdlog::init_thread_pool(8192, 1);
     // Аналогично, напишите тест для метода BuildConfig
     managerBuilder->BuildLogger();
     ASSERT_NE(managerBuilder->BuildConfig("base.json"), nullptr);
 }
 
 TEST_F(ManagerBuilderTest, BuildThreadPoolTest) {
+    spdlog::init_thread_pool(8192, 1);
     managerBuilder->BuildLogger();
     managerBuilder->BuildConfig("base.json");
     ASSERT_NE(managerBuilder->BuildThreadPool(), nullptr);
 }
 
 TEST_F(ManagerBuilderTest, ConstructTest) {
+    spdlog::init_thread_pool(8192, 1);
     ASSERT_NE(managerBuilder->Construct("base.json"), nullptr);
 }
