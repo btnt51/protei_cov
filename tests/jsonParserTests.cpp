9f0076ae1627fc3bd1df8a0fa9763c4f8db78ebc
Evgeniy btnt51 <prog@btnt51.ru>
2023-12-02 17:42:58 +0300

    Updated include files, removed autogenerated comments

--- tests/jsonParserTests.cpp
+++ tests/jsonParserTests.cpp
@@ -1,16 +1,16 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "jsonparser.hpp"  // Замените на правильный путь к вашему заголовочному файлу
+#include "jsonParser.hpp"
 
 class JsonParserTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        // Любые предварительные действия, которые нужны для ваших тестов
+
         jsonParser_ = std::make_unique<utility::JsonParser>(nullptr);
     }
 
     void TearDown() override {
-        // Любые завершающие действия после ваших тестов
+
     }
 
     std::unique_ptr<utility::JsonParser> jsonParser_;
@@ -22,16 +22,12 @@ TEST_F(JsonParserTest, ParseTest) {
 }
 
 TEST_F(JsonParserTest, OutputTest) {
-    // Подготовьте тестовые данные
-    jsonParser_->parse("base.json");  // Парсинг тестового файла
-    // Утверждайте, что метод output возвращает ожидаемую строку
+    jsonParser_->parse("base.json");
     EXPECT_EQ(jsonParser_->output(), "RMin: 10\nRMax: 15\nAmountOfOperators: 5\nSizeOfQueue: 7\n");
 }
 
 TEST_F(JsonParserTest, OutputConfigTest) {
-    // Подготовьте тестовые данные
-    jsonParser_->parse("base.json");  // Парсинг тестового файла
-    // Утверждайте, что метод outputConfig возвращает ожидаемую карту
+    jsonParser_->parse("base.json");
     std::map<std::string, int> expectedConfig = {
         {"RMin", 10},
         {"RMax", 15},
