874f90eacb6931b705a1208c8d0b8159111d4e4c
Evgeniy btnt51 <prog@btnt51.ru>
2023-12-02 18:20:55 +0300

    Updated build script, added --parallel

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ mkdir ./build
 cd ./build
 
 if [ -n "$7" ]; then
-  if [ -n "${10}" ]; then
-    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 -DBoost_INCLUDE_DIR=$8 -DBoost_LIBRARY_DIRS=$9 -DBoost_ROOT=${10} ../
+  if [ -n "${11}" ]; then
+    cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 -DBoost_INCLUDE_DIR=$9 -DBoost_LIBRARY_DIRS=${10} -DBoost_ROOT=${11} ../
   else
     cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 -DCMAKE_CXX_COMPILER=$7 ../
   fi
@@ -13,8 +13,13 @@ else
   cmake -DPATCH_VERSION=$1 -DCMAKE_BUILD_TYPE=$2 -DWITH_TESTS=$3 -DLIGHTWEIGHTBABY=$4 ../
 fi
 
-cmake --build .
-cmake --build . --target $5
+if [ -n "${8}" ]; then
+  cmake --build . --parallel ${8}
+  cmake --build . --target $5 --parallel ${8}
+else
+  cmake --build .
+  cmake --build . --target $5
+fi
 
 if [ -n "$6" ] && [ "$6" != "test" ]; then
   cmake -E tar cfv ./$6.zip --format=7zip ./$5
